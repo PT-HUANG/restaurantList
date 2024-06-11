025afb7b9389c5f1bfaa197ee7770d16bedc8971
PT-HUANG <qwe1593574268@gmail.com>
2024-06-11 08:37:20 +0800

    build: add bootstrap static files for style

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { engine } = require('express-handlebars') //require Handlebar
 const app = express()
 const port = 3000
 
@@ -8,7 +9,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurants', (req, res) => {
-  res.send('show me restaurants')
+  res.render('index')
 })
 
 app.get('/restaurant/:id', (req, res) => {
@@ -23,3 +24,8 @@ app.listen(port, () => {
 
 // Model
 app.use(express.static('public'))
+
+// View
+app.engine('.hbs', engine({extname: '.hbs'})) 
+app.set('view engine', '.hbs')
+app.set('views', './views')
