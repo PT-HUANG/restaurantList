90c672f714b3f11182cfa7b0af5a165bd778b607
PT-HUANG <qwe1593574268@gmail.com>
2024-06-11 08:22:21 +0800

    build: import express and routing setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Controller (Routing)
+app.get('/', (req, res) => {
+  res.redirect('/restaurants')
+})
+
+app.get('/restaurants', (req, res) => {
+  res.send('show me restaurants')
+})
+
+app.get('/restaurant/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`now we are in id: ${id}`)
+})
+
+app.listen(port, () => {
+  console.log(`express server is running on http://localhost:${port}`)
+  //確保伺服器有在正常運行
+})
+
